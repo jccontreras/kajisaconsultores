cba4ed90a21a532a2270f4fbbb4a55b5d23eefec
Ragnarok <juankcontreras00@gmail.com>
2020-07-29 23:50:50 -0500

    Update Web Page and Functions

--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -1,35 +1,47 @@
 <template>
-  <div class="accordion" id="accordionExample">
-    <div class="card cardhome">
-      <div class="card-header">
-        <h5>Contactanos</h5>
+  <div class="card cardhome">
+    <div class="card-header">
+      <h5>Contactanos</h5>
+    </div>
+    <div class="card-body">
+      <div class="alert alert-danger" role="alert" v-if="error">
+        <h4 class="alert-heading">Ups!</h4>
+        <label>Hubo un problema al enviar tu requerimiento</label>
+        <hr>
+        <label class="mb-0">Por favor intentalo de nuevo más tarde.</label>
       </div>
-      <div class="card-body">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Correo Electrónico</label>
-            <input type="email" class="form-control" id="exampleInputEmail1"
-                   aria-describedby="emailHelp"
-                   placeholder="name@correo.com">
-            <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo con
-              nadie
-              más.</small>
-          </div>
-          <div class="form-group">
-            <label>Nombre Completo</label>
-            <input type="text" class="form-control">
-          </div>
-          <div class="form-group">
-            <label>Número de contacto</label>
-            <input type="text" class="form-control">
-          </div>
-          <div class="form-group">
-            <label>Cuéntanos tus problemas</label>
-            <textarea class="form-control" aria-label="With textarea"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary" id="send">Enviar</button>
-        </form>
+      <div class="alert alert-success" role="alert" v-if="sent">
+        <h4 class="alert-heading">Listo!</h4>
+        <label>Tenemos tu requerimiento</label>
+        <hr>
+        <label class="mb-0">Nos pondremos en contacto lo más pronto posible.</label>
       </div>
+      <form @submit.prevent="send" v-else>
+        <div class="form-group">
+          <label for="email">Correo Electrónico</label>
+          <input type="email" class="form-control" id="email"
+                 placeholder="name@correo.com" autocomplete="off" v-model="contact.email" required>
+          <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo con
+            nadie
+            más.</small>
+        </div>
+        <div class="form-group">
+          <label for="name">Nombre Completo</label>
+          <input type="text" class="form-control" id="name" autocomplete="off"
+                 v-model="contact.name" required>
+        </div>
+        <div class="form-group">
+          <label for="phone">Número de contacto</label>
+          <input type="tel" class="form-control" id="phone" autocomplete="off"
+                 v-model="contact.phone" required>
+        </div>
+        <div class="form-group">
+          <label for="trouble">Cuéntanos tus problemas</label>
+          <textarea class="form-control" id="trouble" autocomplete="off"
+                    v-model="contact.trouble" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" id="enviar">Enviar</button>
+      </form>
     </div>
   </div>
 </template>
@@ -37,6 +49,51 @@
 <script>
 export default {
   name: 'Contact',
+  data() {
+    return {
+      sent: false,
+      error: false,
+      contact: {
+        name: null,
+        email: null,
+        phone: null,
+        trouble: null,
+      },
+    };
+  },
+  watch: {
+    sent(val) {
+      setTimeout(() => {
+        if (val) this.sent = false;
+      }, 3000);
+    },
+    error(val) {
+      setTimeout(() => {
+        if (val) this.error = false;
+      }, 3000);
+    },
+  },
+  methods: {
+    send() {
+      this.$fb.functions().httpsCallable('sendForm')({
+        subject: 'Nuevo Mensaje de la PáginaWeb',
+        text: `Please contact ${this.contact.name} at ${this.contact.email}. Their problem is ${this.contact.trouble}. Their phone is ${this.contact.phone}.`,
+        html: `Por favor contactar a <strong>${this.contact.name}</strong> al correo <strong>${this.contact.email}</strong>. <br/> Tiene el siguiente problema: <br/> <strong>${this.contact.trouble}</strong> .<br/>Su número telefonico es: <strong>${this.contact.phone}</strong>`,
+      })
+        .then(() => {
+          this.sent = true;
+        })
+        .catch(() => {
+          this.error = true;
+        })
+        .finally(() => {
+          this.contact.name = null;
+          this.contact.email = null;
+          this.contact.phone = null;
+          this.contact.trouble = null;
+        });
+    },
+  },
 };
 </script>
 
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyCeoIvphD7-C0Jn2LjRJCtGQzZwuxjTcUI',
+  authDomain: 'kajisaconsultores-6a40c.firebaseapp.com',
+  databaseURL: 'https://kajisaconsultores-6a40c.firebaseio.com',
+  projectId: 'kajisaconsultores-6a40c',
+  storageBucket: 'kajisaconsultores-6a40c.appspot.com',
+  messagingSenderId: '273522402070',
+  appId: '1:273522402070:web:911f44dc9cb4c9b97be637',
+  measurementId: 'G-S1RFCXC9T4',
+};
