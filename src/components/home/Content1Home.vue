70a0efa5ceb8f3afb9ed4c1c32fc14625e6d3501
ragnarok <juankcontreras00@gmail.com>
2020-09-22 09:57:35 -0500

    Update pub13

--- src/components/home/Content1Home.vue
+++ src/components/home/Content1Home.vue
@@ -11,9 +11,13 @@
       <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
+        <img src="@/assets/publicidad/pub13.jpeg" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
         <img src="@/assets/publicidad/pub12.jpeg" class="d-block w-100">
       </div>
       <div class="carousel-item">
