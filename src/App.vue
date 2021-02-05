f9f085097b70d93bae4e0345dae46a66c1fc1125
ragnarok <juankcontreras00@gmail.com>
2021-02-05 12:32:20 -0500

    update menu

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app" class="container-fluid">
+    <div class="col align-self-end" style="text-align: end">
+      <a class="navbar-brand" href="https://www.facebook.com/kajisaconsultores/" target="_blank">
+        <img src="@/assets/logo/Facebook.png" width="24" height="24" alt="">
+      </a>
+      <a class="navbar-brand"
+         href="https://www.instagram.com/kajisaconsultores/?igshid=cojsssbm4k6h&fbclid=IwAR31-v3BbD6XKuAxnWIJcic_zzKQ4n1ZB6eiXGgRjWbtY_BvvVX_36EE4fY"
+         target="_blank">
+        <img src="@/assets/logo/Instagram.png" width="24" height="24" alt="">
+      </a>
+      <a class="navbar-brand"
+         href="https://twitter.com/KajisaConsultor?s=08&fbclid=IwAR3wptceW3-GToO2nzQTyl0BoAhhXsmvkGJez5g0_j9X-BrwvzlWFN1nskY"
+         target="_blank">
+        <img src="@/assets/logo/Twitter.png" width="24" height="24" alt="">
+      </a>
+      <a class="navbar-brand"
+         href="https://www.linkedin.com/company/kajisa-consultores"
+         target="_blank">
+        <img src="@/assets/logo/Linckedin.png" width="24" height="24" alt="">
+      </a>
+      <a class="navbar-brand" href="https://www.youtube.com/channel/UCFjbIJsqnmMAseB-vX8wGvg" target="_blank">
+        <img src="@/assets/logo/Youtube.png" width="24" height="24" alt="">
+      </a>
+    </div>
     <div class="row">
-      <div v-if="isOpen" class="col-2" style="max-width: 50%"><top/></div>
+      <div id="top" class="col-2" style="max-width: 50%">
+        <top/>
+      </div>
       <div class="col" style="top: 20px">
-        <button v-if="isOpen" class="navbar-toggler" type="button" data-toggle="collapse"
+        <button v-if="isclose" class="navbar-toggler" type="button" data-toggle="collapse"
            data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
                 aria-expanded="false" aria-label="Toggle navigation" style="position: relative"
-                @click="openMenu">
-          <img src="@/assets/logo/closeMenu.png" style="max-height: 30px; max-width: 30px">
+                @click="openMenuJQ">
+          <img src="@/assets/logo/openicon.png" style="max-height: 30px; max-width: 30px">
         </button>
         <button v-else class="navbar-toggler" type="button" data-toggle="collapse"
           data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
           aria-expanded="false" aria-label="Toggle navigation" style="position: relative"
-          @click="openMenu">
-          <img src="@/assets/logo/openMenu.png" style="max-height: 30px; max-width: 30px">
+          @click="openMenuJQ">
+          <img src="@/assets/logo/closeicon.png" style="max-height: 30px; max-width: 30px">
         </button>
         <router-view/>
       </div>
@@ -27,8 +52,8 @@
 import Top from '@/components/menu/Top.vue';
 import Down from '@/components/footer/Down.vue';
 import ModalAbeasData from '@/components/abeasdata/ModalAbeasData.vue';
-import { mapMutations, mapState } from 'vuex';
-import * as constants from '@/store/constants';
+
+const jq = require('jquery');
 
 export default {
   name: 'App',
@@ -37,16 +62,23 @@ export default {
     Down,
     ModalAbeasData,
   },
-  computed: {
-    ...mapState({
-      isOpen: (state) => state.Menu.isopen,
-    }),
+  data() {
+    return {
+      isclose: false,
+    };
   },
   methods: {
-    ...mapMutations({
-      openMenu: constants.OPEN_MENU,
-      closeMenu: constants.CLOSE_MENU,
-    }),
+    openMenuJQ() {
+      if (this.isclose === true) {
+        jq('#top')
+          .show(500);
+        this.isclose = false;
+      } else {
+        jq('#top')
+          .hide(500);
+        this.isclose = true;
+      }
+    },
   },
   mounted() {
     // eslint-disable-next-line no-undef
--- src/components/menu/Top.vue
+++ src/components/menu/Top.vue
@@ -36,29 +36,6 @@
         </router-link>
       </li>
     </ul>
-    <div>
-      <a class="navbar-brand" href="https://www.facebook.com/kajisaconsultores/" target="_blank">
-        <img src="@/assets/logo/Facebook.png" width="24" height="24" alt="">
-      </a>
-      <a class="navbar-brand"
-         href="https://www.instagram.com/kajisaconsultores/?igshid=cojsssbm4k6h&fbclid=IwAR31-v3BbD6XKuAxnWIJcic_zzKQ4n1ZB6eiXGgRjWbtY_BvvVX_36EE4fY"
-         target="_blank">
-        <img src="@/assets/logo/Instagram.png" width="24" height="24" alt="">
-      </a>
-      <a class="navbar-brand"
-         href="https://twitter.com/KajisaConsultor?s=08&fbclid=IwAR3wptceW3-GToO2nzQTyl0BoAhhXsmvkGJez5g0_j9X-BrwvzlWFN1nskY"
-         target="_blank">
-        <img src="@/assets/logo/Twitter.png" width="24" height="24" alt="">
-      </a>
-      <a class="navbar-brand"
-         href="https://www.linkedin.com/company/kajisa-consultores"
-         target="_blank">
-        <img src="@/assets/logo/Linckedin.png" width="24" height="24" alt="">
-      </a>
-      <a class="navbar-brand" href="https://www.youtube.com/channel/UCFjbIJsqnmMAseB-vX8wGvg" target="_blank">
-        <img src="@/assets/logo/Youtube.png" width="24" height="24" alt="">
-      </a>
-    </div>
   </div>
   <!--nav class="navbar navbar-expand-lg navbar-light colorttop">
     <router-link class="nav-link" :to="{name: 'home'}">
