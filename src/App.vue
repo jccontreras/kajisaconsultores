a427df98661d8906fdfbe28d188af89d6e86109d
ragnarok <juankcontreras00@gmail.com>
2021-08-26 21:50:34 -0500

    update whatsapp function

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,9 @@
         <router-view/>
       </div>
     </div>
+    <a title="Contáctanos Ahora!" href="https://api.whatsapp.com/send?phone=573057249133&text=Hola!%20Quisiera%20saber%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios." class="float" target="_blank">
+      <i class="fa fa-whatsapp my-float"></i>
+    </a>
     <down/>
     <modal-abeas-data/>
   </div>
@@ -95,5 +98,29 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #821518;
+}
+
+.float{
+  position:fixed;
+  width:60px;
+  height:60px;
+  bottom:40px;
+  right:40px;
+  background-color:#25d366;
+  color:#FFF;
+  border-radius:50px;
+  text-align:center;
+  font-size:30px;
+  box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+.float:hover {
+  text-decoration: none;
+  color: #25d366;
+  background-color:#fff;
+}
+
+.my-float{
+  margin-top:16px;
 }
 </style>
