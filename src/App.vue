d4ea82b38f120a0d6f52d5046415310297743b1b
ragnarok <juankcontreras00@gmail.com>
2021-09-07 23:37:20 -0500

    update whastapp contact

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,27 @@
         <router-view/>
       </div>
     </div>
-    <a title="Contáctanos Ahora!"
-       href="https://api.whatsapp.com/send?phone=573057249133&text=Hola!%20Quisiera%20saber%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios."
-       class="float" target="_blank">
-      <i class="fa fa-whatsapp my-float"></i>
-    </a>
+    <div id="wabtn">
+      <label class="floatlblof">Oficina</label>
+      <a title="Contáctanos Ahora!"
+         href="https://api.whatsapp.com/send?phone=573057249133&text=Hola!%20Quisiera%20saber%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios."
+         class="float2" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
+      <a class="floatx" @click="waChange" title="Cerrar" style="cursor: pointer">X</a>
+      <label class="floatlbl24">24/7</label>
+      <a title="Contáctanos Ahora!"
+         href="https://api.whatsapp.com/send?phone=573138079966&text=Hola!%20Quisiera%20saber%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios."
+         class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
+    </div>
+    <div id="contactbtn">
+      <a title="Contáctanos Ahora!" style="cursor: pointer"
+         class="float" @click="waChange">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
+    </div>
     <down/>
     <modal-abeas-data/>
   </div>
@@ -48,11 +64,23 @@ export default {
   data() {
     return {
       isclose: false,
+      waisclose: true,
     };
   },
   methods: {
+    waChange() {
+      if (this.waisclose) {
+        jq('#wabtn').show(500);
+        jq('#contactbtn').hide(500);
+        this.waisclose = false;
+      } else {
+        jq('#wabtn').hide(500);
+        jq('#contactbtn').show(500);
+        this.waisclose = true;
+      }
+    },
     openMenuJQ() {
-      if (this.isclose === true) {
+      if (this.isclose) {
         jq('#top')
           .show(500);
         this.isclose = false;
@@ -67,6 +95,7 @@ export default {
     // eslint-disable-next-line no-undef
     $('#staticBackdrop')
       .modal('show');
+    jq('#wabtn').hide(0);
   },
 };
 </script>
@@ -90,7 +119,7 @@ export default {
   position: fixed;
   width: 60px;
   height: 60px;
-  bottom: 40px;
+  bottom: 30px;
   right: 40px;
   background-color: #25d366;
   color: #FFF;
@@ -98,6 +127,57 @@ export default {
   text-align: center;
   font-size: 30px;
   box-shadow: 2px 2px 3px #999;
+  z-index: 100;
+}
+
+.float2 {
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 120px;
+  right: 40px;
+  background-color: #25d366;
+  color: #FFF;
+  border-radius: 50px;
+  text-align: center;
+  font-size: 30px;
+  box-shadow: 2px 2px 3px #999;
+  z-index: 100;
+}
+
+.floatx {
+  position: fixed;
+  width: 10px;
+  height: 10px;
+  bottom: 125px;
+  right: 120px;
+  color: #FFF;
+  text-align: center;
+  font-size: 20px;
+  z-index: 100;
+}
+
+.floatlblof {
+  position: fixed;
+  width: 10px;
+  height: 10px;
+  bottom: 190px;
+  right: 90px;
+  color: #000000;
+  text-align: center;
+  font-size: 20px;
+  z-index: 100;
+}
+
+.floatlbl24 {
+  position: fixed;
+  width: 10px;
+  height: 10px;
+  bottom: 100px;
+  right: 80px;
+  color: #000000;
+  text-align: center;
+  font-size: 20px;
   z-index: 100;
 }
 
@@ -110,4 +190,5 @@ export default {
 .my-float {
   margin-top: 16px;
 }
+
 </style>
